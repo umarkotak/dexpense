581fc4dedb6812d34d8ef23876513437268d28b2
umarkotak <umarkotak@gmail.com>
2021-06-01 19:45:09 +0700

    base pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,13 @@ function Navbar() {
               <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"></i></a>
             </li>
             <li className="nav-item d-none d-sm-inline-block">
-              <a href="#" className="nav-link">Home</a>
+              <a href="#" className="nav-link">Sign Up</a>
+            </li>
+            <li className="nav-item d-none d-sm-inline-block">
+              <a href="#" className="nav-link">Login</a>
+            </li>
+            <li className="nav-item d-none d-sm-inline-block">
+              <a href="#" className="nav-link">Logout</a>
             </li>
           </ul>
 
