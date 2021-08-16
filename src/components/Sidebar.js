c1c3310d751aa7091308528a9aa5327295b4b0ae
umar.ramadhana <umar.ramadhana@gmail.com>
2021-08-16 10:17:43 +0700

    implement dynamic sidebar on not logged in

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -73,7 +73,7 @@ function Sidebar() {
     return(
       <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li className="nav-item">
-          <Link to="/" className="nav-link"><i className="nav-icon fas fa-home"></i> <p>Home</p></Link>
+          <Link to="/" className={`nav-link ${sideBarItems["home"] || ""}`}><i className="nav-icon fas fa-home"></i> <p>Home</p></Link>
         </li>
       </ul>
     )
